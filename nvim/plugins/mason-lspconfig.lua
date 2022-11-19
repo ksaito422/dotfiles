24a8531ccdafba121e2b1efc43363a1ba7f36337
saitooooooo <kitsit.39@gmail.com>
2022-11-19 13:33:49 +0900

    fix: lspのアップデートと遅延読込は非推奨のためやめた

---
+++ nvim/plugins/mason-lspconfig.lua
@@ -0,0 +1,21 @@
+local mason_lsp = require("mason-lspconfig")
+mason_lsp.setup()
+mason_lsp.setup_handlers({ function(server_name)
+  local on_attach = function(client, bufnr)
+    local set = vim.keymap.set
+    set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+    set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+    set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+    set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+    set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+    set('n', 'gx', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
+    set('n', 'g[', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+    set('n', 'g]', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+    set('n', 'J', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+  end
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach
+    }
+  end
+})
+
