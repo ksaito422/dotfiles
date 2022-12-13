07c77656cdb940c3d49850c9398bdf2c3e874bc5
saitooooooo <kitsit.39@gmail.com>
2022-12-13 21:27:09 +0900

    change: glyph-pallette.vim->lua

---
+++ nvim/plugins/glyph-pallette.lua
@@ -0,0 +1,8 @@
+vim.cmd([[
+	augroup my-glyph-palette
+		autocmd! *
+		autocmd FileType fern call glyph_palette#apply()
+		autocmd FileType nerdtree,startify call glyph_palette#apply()
+	augroup END
+]])
+
