af47ff6f351e57148fc52ecb1c136b13651eca4c
saitooooooo <kitsit.39@gmail.com>
2023-01-14 15:20:56 +0900

    update(plugin): todo-comments

--- nvim/lua/plugin_settings/todo-comments.lua
+++ nvim/lua/plugin_settings/todo-comments.lua
@@ -9,7 +9,7 @@ todo_comments.setup {
 	-- keywords recognized as todo comments
 	keywords = {
 		FIX = {
-			icon = " ", -- icon used for the sign, and in search results
+			icon = "", -- icon used for the sign, and in search results
 			color = "error", -- can be a hex color, or a named color (see below)
 			alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
 			-- signs = false, -- configure signs for some keywords individually
