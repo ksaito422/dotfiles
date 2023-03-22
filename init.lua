e2bec85cb87d71f0d987a49b1765a041d0057bfc
saitooooooo <kitsit.39@gmail.com>
2023-03-22 09:41:20 +0900

    chore: weztermの設定をする

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Setup Hammer spoon
--- <ctrl+q>でkittyをアクティブウインドウにする
-hs.hotkey.bind({ "ctrl" }, "q", function()
-	local kitty = hs.application.get("kitty")
-	if kitty == nil then
-		hs.application.launchOrFocus("/Applications/kitty.app")
-	elseif kitty:isFrontmost() then
-		kitty:hide()
+-- <ctrl+q>でweztermをアクティブウインドウにする
+hs.hotkey.bind({ 'CTRL' }, 'q', function()
+	local wez = hs.application.get('wezterm')
+	if wez == nil then
+		hs.application.launchOrFocus('/Applications/wezterm.app')
+	elseif wez:isFrontmost() then
+		wez:hide()
 	else
 		local space = hs.spaces.focusedSpace()
-		local win = kitty:focusedWindow()
+		local win = wez:focusedWindow()
 		hs.spaces.moveWindowToSpace(win, space)
 		win:focus()
 	end
