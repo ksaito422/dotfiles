11d642fc60e3bf755228b7514e3fe33637c97d9d
saitooooooo <kitsit.39@gmail.com>
2023-05-24 15:35:05 +0900

    update: css indent

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -89,6 +89,7 @@ vim.cmd([[
 	autocmd FileType sh setlocal tabstop=2 shiftwidth=2
 	autocmd FileType toml setlocal tabstop=2 shiftwidth=2
 	autocmd FileType markdown setlocal tabstop=2 shiftwidth=2
+	autocmd FileType css setlocal tabstop=2 shiftwidth=2
 ]])
 
 -- 全てのjsonファイルをjsoncとして扱うようにする
