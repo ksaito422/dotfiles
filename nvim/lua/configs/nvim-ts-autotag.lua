65e76bb93e4c4eae57ef5357d7b7f6e58e9f2e4b
saitooooooo <kitsit.39@gmail.com>
2023-06-14 09:04:49 +0900

    chore: erubyでHTMLタグを補完する

--- nvim/lua/configs/nvim-ts-autotag.lua
+++ nvim/lua/configs/nvim-ts-autotag.lua
@@ -3,4 +3,18 @@ if (not status) then
 	return
 end
 
-autotag.setup({})
+autotag.setup({
+	autotag = {
+		enable = true,
+		filetypes = {
+			'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'tsx',
+			'jsx', 'rescript',
+			'xml',
+			'php',
+			'markdown',
+			'astro',
+			'eruby',
+			'embedded_template',
+		}
+	}
+})
