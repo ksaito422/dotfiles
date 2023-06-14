397fa4f6e6493517d0a42eb75eb0b4937139f98d
saitooooooo <kitsit.39@gmail.com>
2023-06-14 21:00:26 +0900

    chore: telescope keybind old_files

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -1,10 +1,11 @@
 local keymap = vim.api.nvim_set_keymap
 local status_ok, telescope = pcall(require, 'telescope.builtin')
 if not status_ok then
-	return
+    return
 end
 
 keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+keymap('n', '<leader>fr', ':Telescope oldfiles<CR>', { noremap = true, silent = true })
 keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
 keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
 keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
