94a34bb265a587dfb9bab61acc8666812ae6b7b2
ksaito <kitsit.39@gmail.com>
2023-07-02 18:30:00 +0900

    chore: lazygit windowを最大にする

--- nvim/lua/configs/lazygit.lua
+++ nvim/lua/configs/lazygit.lua
@@ -7,7 +7,7 @@ keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
 -- transparency of floating window
 g.lazygit_floating_window_winblend = 0
 -- scaling factor for floating window
-g.lazygit_floating_window_scaling_factor = 0.9
+g.lazygit_floating_window_scaling_factor = 1
 -- customize lazygit popup window corner characters
 g.lazygit_floating_window_corner_chars = { '╭', '╮', '╰', '╯' }
 -- use plenary.nvim to manage floating window if available
