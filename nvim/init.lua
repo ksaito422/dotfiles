f014a59a8544d4a04cb7e72196d02919022db3b9
ksaito <kitsit.39@gmail.com>
2023-07-03 21:10:19 +0900

    fix: vscodeの場合nvim pluginをロードしないようにする

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,10 +9,13 @@
 
 require('base')
 require('keymap')
-require('functions')
-require('plugins')
-require('plugins_load')
-require('lsp')
+
+if not vim.g.vscode then
+    require('functions')
+    require('plugins')
+    require('plugins_load')
+    require('lsp')
+end
 
 -- PackerCompileを自動実行
 vim.cmd [[autocmd BufWritePost plugins.lua PackerCompile]]
