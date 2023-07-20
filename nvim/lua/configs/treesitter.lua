9b125ab3a9a8b634f46d481bd6a40ab2ccbfc0fa
ksaito <kitsit.39@gmail.com>
2023-07-20 09:09:50 +0900

    fix: 必要な言語だけインストールする

--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -1,14 +1,61 @@
-local status, nvim_treesitter_configs = pcall(require, 'nvim-treesitter.configs')
-if (not status) then return end
+local status, nvim_treesitter_configs = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
 
-nvim_treesitter_configs.setup {
+nvim_treesitter_configs.setup({
+	-- ensure_installed = "all", -- one of "all", "maintained"
+	ensure_installed = {
+		"bash",
+		"c",
+		"comment",
+		"cpp",
+		"css",
+		"diff",
+		"dockerfile",
+		"git_config",
+		"git_rebase",
+		"gitattributes",
+		"gitcommit",
+		"gitignore",
+		"go",
+		"gomod",
+		"gosum",
+		"graphql",
+		"hcl",
+		"html",
+		"http",
+		"ini",
+		"javascript",
+		"jq",
+		"jsdoc",
+		"json",
+		"json5",
+		"lua",
+		"make",
+		"markdown",
+		"markdown_inline",
+		"mermaid",
+		"ruby",
+		"scss",
+		"sql",
+		"terraform",
+		"toml",
+		"tsx",
+		"typescript",
+		"vim",
+		"vimdoc",
+		"yaml",
+	}, -- one of "all", "maintained"
+	-- ignore_install = { "haskell", "phpdoc" },
 	highlight = {
 		enable = true,
-		disable = {}
+		disable = {},
 	},
 	indent = {
-		enable = true
+		enable = true,
 	},
-	ensure_installed = 'all', -- one of "all", "maintained"
-	ignore_install = { "haskell", "phpdoc" }
-}
+	autotag = {
+		enable = true,
+	},
+})
