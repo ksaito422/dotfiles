99ca91e89397ab8eb46e869dd09081ce27c7290f
ksaito <kitsit.39@gmail.com>
2024-05-06 09:20:21 +0900

    chore(toggleterm): floatに変える

--- nvim/lua/configs/toggleterm.lua
+++ nvim/lua/configs/toggleterm.lua
@@ -30,7 +30,7 @@ toggleterm.setup({
     -- on_stdout = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stdout
     -- on_stderr = fun(t: Terminal, job: number, data: string[], name: string) -- callback for processing output on stderr
     -- on_exit = fun(t: Terminal, job: number, exit_code: number, name: string) -- function to run when terminal process exits
-    -- hide_numbers = true, -- hide the number column in toggleterm buffers
+    -- hide_numbers = true, -- hide the number column in toggleterm buffers--[[  ]]
     -- shade_filetypes = {},
     -- autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
     -- highlights = {
@@ -54,24 +54,24 @@ toggleterm.setup({
     -- terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
     -- persist_size = true,
     -- persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-    direction = 'horizontal' -- 'vertical' | 'horizontal' | 'tab' | 'float',
+    direction = 'float', -- 'vertical' | 'horizontal' | 'tab' | 'float',
     -- close_on_exit = true, -- close the terminal window when the process exits
     -- -- Change the default shell. Can be a string or a function returning a string
     -- shell = vim.o.shell,
     -- auto_scroll = true, -- automatically scroll to the bottom on terminal output
     -- -- This field is only relevant if direction is set to 'float'
-    -- float_opts = {
+    float_opts = {
     --     -- The border key is *almost* the same as 'nvim_open_win'
     --     -- see :h nvim_open_win for details on borders however
     --     -- the 'curved' border is a custom border type
     --     -- not natively supported but implemented in this plugin.
-    --     border = 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+        border = 'single', --'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
     --     -- like `size`, width and height can be a number or function which is passed the current terminal
     --     width = <value>,
     --     height = <value>,
-    --     winblend = 3,
+        winblend = 10,
     --     zindex = <value>,
-    -- },
+    },
     -- winbar = {
     --     enabled = false,
     --     name_formatter = function(term) --  term: Terminal
