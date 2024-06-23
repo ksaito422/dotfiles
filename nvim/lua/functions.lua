6981aab9d0254161258150317dc759053326f82a
ksaito <kitsit.39@gmail.com>
2024-06-23 13:16:47 +0900

    style

--- nvim/lua/functions.lua
+++ nvim/lua/functions.lua
@@ -6,7 +6,7 @@ nvim_create_user_command('DisableIme', function()
     vim.cmd([[
 	function! s:disableIme()
 		if has('mac')
-			call jobstart(['osascript', '-e', 'tell application "System Events" to key code {102}'], {'in_io': 'null', 'out_io': 'null', 'err_io': 'null'})		
+			call jobstart(['osascript', '-e', 'tell application "System Events" to key code {102}'], {'in_io': 'null', 'out_io': 'null', 'err_io': 'null'})
 		endif
 	endfunction
 ]])
