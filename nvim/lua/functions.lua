3e8734fdd2b09adcf4033a5ef58852cef32b3ec2
ksaito <kitsit.39@gmail.com>
2024-06-23 13:16:52 +0900

    chore(nvim): 行番号の表示を相対と絶対で切り替えるコマンドを登録する

--- nvim/lua/functions.lua
+++ nvim/lua/functions.lua
@@ -11,3 +11,16 @@ nvim_create_user_command('DisableIme', function()
 	endfunction
 ]])
 end, {})
+
+-- 行番号の表示を相対と絶対で切り替える
+function ToggleRelativeNumber()
+  if vim.wo.relativenumber then
+    vim.wo.relativenumber = false
+    vim.wo.number = true
+  else
+    vim.wo.relativenumber = true
+    vim.wo.number = true
+  end
+end
+vim.api.nvim_create_user_command('ToggleRelNum', ToggleRelativeNumber, {})
+vim.api.nvim_set_keymap('n', '<F2>', ':ToggleRelNum<CR>', { noremap = true, silent = true })
