64ef832a5e77760d49ace60a17c06c80dbd49682
ksaito <kitsit.39@gmail.com>
2024-09-18 08:28:49 +0900

    style: 調整

--- nvim/lua/configs/nvim-lspconfig.lua
+++ nvim/lua/configs/nvim-lspconfig.lua
@@ -62,18 +62,10 @@ nvim_lsp.lua_ls.setup({
   on_attach = on_attach,
   settings = {
     Lua = {
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = false,
-      },
-      diagnostics = {
-        enable = false,
-      },
-      format = {
-        enable = false,
-      },
+      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+      telemetry = { enable = false },
+      diagnostics = { enable = false },
+      format = { enable = false },
     },
   },
 })
