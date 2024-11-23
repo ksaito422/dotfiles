fcb5ffdec82d0e1c4fc4420ca86f5db9450ed6fd
ksaito <kitsit.39@gmail.com>
2024-11-23 10:22:30 +0900

    remove deol.vim

--- nvim/lua/configs/deol.lua
+++
@@ -1,5 +0,0 @@
-local keymap = vim.api.nvim_set_keymap
-
-keymap("n", "<C-t>", ":Deol -split=floating -winwidth=200 -winheight=150<CR>", { noremap = true, silent = true })
-keymap("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
-
