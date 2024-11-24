710bd53dcea740cb2e70e104fe3666bbec73c77e
ksaito <kitsit.39@gmail.com>
2024-11-24 14:09:59 +0900

    insert mode抜けるときにIMEをローマ字入力に切り替える

--- nvim/lua/functions.lua
+++ nvim/lua/functions.lua
@@ -1,16 +1,10 @@
-local nvim_create_user_command = vim.api.nvim_create_user_command
-
--- TODO: 直す
--- Normal mode切り替え時に日本語入力をoffにする
-nvim_create_user_command("DisableIme", function()
-  vim.cmd([[
-	function! s:disableIme()
-		if has('mac')
-			call jobstart(['osascript', '-e', 'tell application "System Events" to key code {102}'], {'in_io': 'null', 'out_io': 'null', 'err_io': 'null'})
-		endif
-	endfunction
-]])
-end, {})
+-- Insert modeを抜ける時にIMEをローマ字入力に変える
+-- dependencies: im-select
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    vim.fn.system("im-select com.google.inputmethod.Japanese.Roman")
+  end,
+})
 
 -- 行番号の表示を相対と絶対で切り替える
 function ToggleRelativeNumber()
