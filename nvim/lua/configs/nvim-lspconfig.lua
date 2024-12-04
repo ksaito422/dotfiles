579789fc83edf140ebe4d89b4c96c1fe52c0e2ca
ksaito <kitsit.39@gmail.com>
2024-12-04 21:30:55 +0900

    chore: lsp command

--- nvim/lua/configs/nvim-lspconfig.lua
+++ nvim/lua/configs/nvim-lspconfig.lua
@@ -22,10 +22,14 @@ local on_attach = function()
 end
 
 -- NOTE: lsp sagaにもlsp関連のコマンドあり
+keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
 keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
 keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
 keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+-- keymap("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
+keymap("n", "gr", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
+-- keymap("n", "go", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", { noremap = true, silent = true })
 keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
 keymap("n", "<space><space>", "<cmd>lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
 
