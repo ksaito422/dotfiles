aab8c4a115f427ac8e7bd96f91fbc226fde1e611
ksaito <kitsit.39@gmail.com>
2024-12-07 08:59:57 +0900

    chore: dashboardにgithub操作を追加

--- nvim/lua/configs/alpha.lua
+++ nvim/lua/configs/alpha.lua
@@ -18,8 +18,13 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-  dashboard.button("f", "󰱽  Find file", ":Telescope find_files <CR>"),
-  dashboard.button("g", "󰱼  Find git managed file", ":Telescope git_files <CR>"),
+  dashboard.button("ff", "󰱽  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("fg", "󰱼  Find git managed file", ":Telescope git_files <CR>"),
+  dashboard.button("vr", "  Recently used files", ":Telescope oldfiles <CR>"),
+  dashboard.button("fl", "󰊄  Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("op", "  Seacrh PR", ":Octo pr search<CR>"),
+  dashboard.button("oa", "  Seacrh PR author:@me is:open", ":Octo pr search author:@me is:open<CR>"),
+  dashboard.button("or", "  Seacrh PR review requested:@me is:open", ":Octo pr search review-requested:@me is:open<CR>"),
   dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
   dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
