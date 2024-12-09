80378d23fd1f41009d9d89148abc22417a72be8a
ksaito <kitsit.39@gmail.com>
2024-12-09 09:06:22 +0900

    chore: add command

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -49,7 +49,7 @@ end)(vim.lsp.util.open_floating_preview)
 local keymap = vim.api.nvim_set_keymap
 keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
 keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
-keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+-- keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
 keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
 keymap("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
@@ -57,6 +57,7 @@ keymap("n", "gr", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent 
 keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
 keymap("n", "<space><space>", "<cmd>lua vim.lsp.buf.format()<CR>", { noremap = true, silent = true })
 
+keymap("n", "gd", "<cmd>Trouble lsp_definitions toggle<CR>", { noremap = true, silent = true })
 keymap("n", "gh", "<cmd>Trouble lsp_references toggle<CR>", { noremap = true, silent = true })
 keymap("n", "gs", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", { noremap = true, silent = true })
 keymap("n", "go", "<cmd>Trouble symbols toggle<CR>", { noremap = true, silent = true })
