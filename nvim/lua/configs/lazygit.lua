61b9abd7f425ac58f11a9a8e0d00c461b81c8532
ksaito <kitsit.39@gmail.com>
2024-12-11 09:11:51 +0900

    chore: update lazygit ui

--- nvim/lua/configs/lazygit.lua
+++ nvim/lua/configs/lazygit.lua
@@ -7,7 +7,7 @@ keymap("n", "<leader>gl", ":LazyGitFilter<CR>", { noremap = true, silent = true 
 keymap("n", "<leader>glc", ":LazyGitFilterCurrentFile<CR>", { noremap = true, silent = true })
 
 -- transparency of floating window
-g.lazygit_floating_window_winblend = 0
+g.lazygit_floating_window_winblend = 10
 -- scaling factor for floating window
 g.lazygit_floating_window_scaling_factor = 0.95
 -- customize lazygit popup window corner characters
@@ -18,3 +18,8 @@ end
 g.lazygit_floating_window_use_plenary = 0
 -- fallback to 0 if neovim-remote is not installed
 g.lazygit_use_neovim_remote = 1
+
+vim.cmd([[
+  highlight LazyGitFloat guibg=#1e222a
+  highlight LazyGitBorder guifg=#999fff
+  ]])
