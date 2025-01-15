b95f5b47a373943803838e3362346a3e838d6caa
ksaito <kitsit.39@gmail.com>
2025-01-15 19:49:28 +0900

    style:

--- nvim/lua/configs/memolist.lua
+++ nvim/lua/configs/memolist.lua
@@ -1,5 +1,6 @@
-vim.g.memolist_path="~/.memolist"
-vim.g.memolist_memo_suffix="md"
-vim.api.nvim_set_keymap("n", "<Leader>mn", ":MemoNew<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>ml", ":MemoList<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<Leader>mg", ":MemoGrep<CR>", {noremap = true, silent = true})
+vim.g.memolist_path = "~/.memolist"
+vim.g.memolist_memo_suffix = "md"
+vim.api.nvim_set_keymap("n", "<Leader>mn", ":MemoNew<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>ml", ":MemoList<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>mg", ":MemoGrep<CR>", { noremap = true, silent = true })
+
