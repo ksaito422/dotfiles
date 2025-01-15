165d870ee55188ce265086b9de5eb397e412cb8a
ksaito <kitsit.39@gmail.com>
2025-01-15 20:16:17 +0900

    Revert "fix: telescope preview width"
    This reverts commit bbb2d57e599045f2a57fc7a10c2da90c2fe04315.

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -49,6 +49,7 @@ telescope.setup({
       height = 0.8,
       width = 0.8,
       prompt_position = "top",
+      preview_width = 0.5,
     },
     file_ignore_patterns = { "^.git/", "^node_modules/" },
     mappings = {
@@ -84,9 +85,6 @@ telescope.setup({
     },
     buffers = {
       prompt_title = "Find Buffers",
-      layout_config = {
-        preview_width = 0.5,
-      },
     },
     diagnostics = {
       layout_strategy = "vertical",
@@ -126,9 +124,6 @@ telescope.setup({
           ["h"] = fb_actions.toggle_hidden,
           ["s"] = fb_actions.toggle_all,
         },
-      },
-      layout_config = {
-        preview_width = 0.5,
       },
     },
   },
