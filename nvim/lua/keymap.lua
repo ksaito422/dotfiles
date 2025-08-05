8d498348a7d8769d378bb90692e96b673a639644
ksaito <kitsit.39@gmail.com>
2025-08-06 08:14:45 +0900

    <C-c>で作業中のディレクトリからのパスを取得する

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -30,7 +30,7 @@ keymap("t", "<C-w>", [[<C-\><C-n><C-w>]], { noremap = true, silent = true })
 keymap("n", "<C-[>", ":bprev<CR>", { noremap = true, silent = true })
 keymap("n", "<C-]>", ":bnext<CR>", { noremap = true, silent = true })
 
-keymap("n", "<C-c>", ":let @* = expand('%:p')<CR>", { noremap = true })
+keymap("n", "<C-c>", ":let @* = expand('%')<CR>", { noremap = true })
 
 vim.api.nvim_create_user_command("T", function(opts)
   vim.cmd("split")
