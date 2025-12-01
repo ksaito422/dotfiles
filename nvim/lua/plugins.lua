7ee10bc7e8b9368c2f478bdb2286bd5e67561c45
ksaito <kitsit.39@gmail.com>
2025-12-02 08:32:07 +0900

    install overlook.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -292,6 +292,35 @@ require("lazy").setup({
     requires = "neovim/nvim-lspconfig",
   },
 
+  -- コードジャンプをポップアップで表示
+  {
+    "WilliamHsieh/overlook.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>pd",
+        function()
+          require("overlook.api").peek_definition()
+        end,
+        desc = "Overlook: Peek definition",
+      },
+      {
+        "<leader>pc",
+        function()
+          require("overlook.api").close_all()
+        end,
+        desc = "Overlook: Close all popup",
+      },
+      {
+        "<leader>pu",
+        function()
+          require("overlook.api").restore_popup()
+        end,
+        desc = "Overlook: Restore popup",
+      },
+    },
+  },
+
   -- lspのセットアップ状況の可視化と通知を見やすくする
   {
     "j-hui/fidget.nvim",
