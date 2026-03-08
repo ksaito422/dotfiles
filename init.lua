6d0e805a88b8e9de080fc9edfb5edb43341f8134
ksaito <kitsit.39@gmail.com>
2026-03-08 20:04:16 +0900

    使わないアプリをホットキーから外す

--- init.lua
+++ init.lua
@@ -14,35 +14,5 @@ hs.hotkey.bind({ "CTRL" }, "q", function()
   end
 end)
 
--- <ctrl+x>でvscodeをアクティブウインドウにする
-hs.hotkey.bind({ "CTRL" }, "x", function()
-  local vscode = hs.application.get("Code")
-  if vscode == nil then
-    hs.application.launchOrFocus("/Applications/Visual Studio Code.app")
-  elseif vscode:isFrontmost() then
-    vscode:hide()
-  else
-    local space = hs.spaces.focusedSpace()
-    local win = vscode:focusedWindow()
-    hs.spaces.moveWindowToSpace(win, space)
-    win:focus()
-  end
-end)
-
--- <ctrl+s>でobsidianをアクティブウインドウにする
-hs.hotkey.bind({ "CTRL" }, "s", function()
-  local obsidian = hs.application.get("Obsidian")
-  if obsidian == nil then
-    hs.application.launchOrFocus("/Applications/Obsidian.app")
-  elseif obsidian:isFrontmost() then
-    obsidian:hide()
-  else
-    local space = hs.spaces.focusedSpace()
-    local win = obsidian:focusedWindow()
-    hs.spaces.moveWindowToSpace(win, space)
-    win:focus()
-  end
-end)
-
 hs.loadSpoon("ShiftIt")
 spoon.ShiftIt:bindHotkeys({})
