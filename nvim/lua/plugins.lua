0cde38ff1092409cd64edaa2ababa622589d24bd
ksaito <kitsit.39@gmail.com>
2026-03-15 20:29:36 +0900

    upgrade telescope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -159,7 +159,6 @@ require("lazy").setup({
   -- ファジーファインダー
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.4",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-live-grep-args.nvim",
