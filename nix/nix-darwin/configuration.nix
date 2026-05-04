35a25cf6783ed85b10c48ba99c12163c108c18a4
ksaito <kitsit.39@gmail.com>
2026-05-04 10:27:26 +0900

    init nix-darwin

---
+++ nix/nix-darwin/configuration.nix
@@ -0,0 +1,12 @@
+{ self, ... }:
+
+{
+  users.users."saito".home = "/Users/saito";
+
+  imports = [ ./home_manager.nix ];
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  system.stateVersion = 6;
+  nix.enable = false;
+  security.pam.services.sudo_local.touchIdAuth = true;
+}
