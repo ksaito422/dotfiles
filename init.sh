3fdaf4fb8566ca042cfe331877e05f31c3229a31
ksaito <kitsit.39@gmail.com>
2026-09-20 14:44:13 +0900

    update init.sh

--- init.sh
+++ init.sh
@@ -19,6 +19,9 @@ fi
 # miseのインストール
 curl -fsSL https://mise.run | sh
 
+# claude codeのインストール
+curl -fsSL https://claude.ai/install.sh | bash
+
 # dotfilesに戻る
 cd $path
 
@@ -40,16 +43,16 @@ if [ $path ]; then
   ln -s $path/yazi $HOME/.config/yazi
   # karabiner
   ln -s $path/karabiner/karabiner $HOME/.config/karabiner/karabiner.json
+  # claude code
+  ln -s $path/.claude/skills $HOME/.claude/skills
+  ln -s $path/.claude/agents $HOME/.claude/agents
+  ln -s $path/.claude/settings.json $HOME/.claude/settings.json
 fi
-
-# brew install
-cd $path/brew
-brew bundle --global
 
 # nerdfontのインストール
 git clone --branch=master --depth 1 https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
 ./install.sh
 cd ..
-$ rm -rf nerd-fonts
+rm -rf nerd-fonts
 
